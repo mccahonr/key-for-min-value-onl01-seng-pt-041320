a0855108b2505074423b8a074c0f3c9de3aa06ee
Ryan McCahon <mccahonr@yahoo.com>
2020-04-24 19:48:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   lowest_key = nil
   name_hash.each do |key, value|
-    if lowest_value
+    if lowest_value == nil || lowest_value < value
+      
     
     
 end
